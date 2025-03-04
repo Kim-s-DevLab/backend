c56762d93a9459067bc33604638439b8fbedfdd4
ijnim1121 <jimin3347@naver.com>
2025-03-04 14:35:53 +0900

    Initial commit

---
+++ src/main/java/eightplusone/bit/fit/global/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package eightplusone.bit.fit.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+			.csrf(AbstractHttpConfigurer::disable)
+			.authorizeHttpRequests(auth -> auth
+				.anyRequest().permitAll() // 모든 요청 허용
+			)
+			.formLogin(AbstractHttpConfigurer::disable) // 로그인 폼 비활성화
+			.httpBasic(AbstractHttpConfigurer::disable); // 기본 인증 비활성화
+
+		return http.build();
+	}
+}
