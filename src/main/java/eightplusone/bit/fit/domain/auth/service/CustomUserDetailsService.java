7548098e34c91dc7b0fc525d51de879429d985c8
조영무 <fprh13@gmail.com>
2025-03-06 13:20:39 +0900

    feat: 시큐리티 권한 객체 구현 #4

---
+++ src/main/java/eightplusone/bit/fit/domain/auth/service/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package eightplusone.bit.fit.domain.auth.service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+
+import eightplusone.bit.fit.domain.auth.dto.CustomUserDetails;
+import eightplusone.bit.fit.domain.user.entity.User;
+import eightplusone.bit.fit.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+	private final UserRepository userRepository;
+
+	@Override
+	public CustomUserDetails loadUserByUsername(String loginId) {
+		User findUser = userRepository.findLoginUserByEmail(loginId);
+		if (findUser != null) {
+			return new CustomUserDetails(findUser);
+		}
+		return null;
+	}
+}
--- src/main/java/eightplusone/bit/fit/domain/user/repository/UserRepository.java
+++ src/main/java/eightplusone/bit/fit/domain/user/repository/UserRepository.java
@@ -5,4 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import eightplusone.bit.fit.domain.user.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+	boolean existsByProvider(String provider);
+
+	boolean existsByEmail(String email);
+
+	User findLoginUserByEmail(String email);
 }
