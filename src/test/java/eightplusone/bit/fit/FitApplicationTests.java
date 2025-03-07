0f1fda2b6558fcaf0a9d924141ca0aca87ce8d03
Hm-source <swo8852@gmail.com>
2025-03-07 13:55:29 +0900

    test: FitApplicationTests 클래스만 컨텍스트에 로드 원복

--- src/test/java/eightplusone/bit/fit/FitApplicationTests.java
+++ src/test/java/eightplusone/bit/fit/FitApplicationTests.java
@@ -3,7 +3,7 @@ package eightplusone.bit.fit;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = FitApplicationTests.class)
+@SpringBootTest
 class FitApplicationTests {
 
 	@Test
