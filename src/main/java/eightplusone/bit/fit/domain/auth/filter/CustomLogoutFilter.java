8f14d50ee6d67ee9240728fed617dfef6476b406
조영무 <fprh13@gmail.com>
2025-03-07 10:27:38 +0900

    feat: 로그아웃 기능 구현 #4

---
+++ src/main/java/eightplusone/bit/fit/domain/auth/filter/CustomLogoutFilter.java
@@ -0,0 +1,68 @@
+package eightplusone.bit.fit.domain.auth.filter;
+
+import static eightplusone.bit.fit.global.constants.TokenConstant.*;
+
+import java.io.IOException;
+import java.util.Date;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.filter.GenericFilterBean;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import eightplusone.bit.fit.domain.auth.jwt.TokenProvider;
+import eightplusone.bit.fit.global.utils.CookieUtil;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class CustomLogoutFilter extends GenericFilterBean {
+
+	private final TokenProvider tokenProvider;
+	private final ObjectMapper objectMapper;
+
+	public CustomLogoutFilter(TokenProvider tokenProvider, ObjectMapper objectMapper) {
+		this.tokenProvider = tokenProvider;
+		this.objectMapper = objectMapper;
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws
+		IOException,
+		ServletException {
+		doFilter((HttpServletRequest)request, (HttpServletResponse)response, chain);
+	}
+
+	private void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws
+		IOException,
+		ServletException {
+		if (!request.getRequestURI().equals("/auth/logout") || !request.getMethod().equals("POST")) {
+			filterChain.doFilter(request, response);
+			return;
+		}
+
+		String accessToken = tokenProvider.resolveAccessToken(request);
+		Claims claimsByAccessToken = tokenProvider.getClaimsByAccessToken(accessToken);
+
+		String id = claimsByAccessToken.getSubject();
+		String role = claimsByAccessToken.get(AUTHORITIES_KEY).toString();
+
+		tokenProvider.invalidateRefreshToken(id);
+
+		log.info("{}-{}: logout ({})", id, role, new Date());
+		response.addHeader(HttpHeaders.SET_COOKIE,
+			CookieUtil.createCookie(REFRESH_TOKEN_COOKIE_NAME, null, REFRESH_EXPIRATION_DELETE).toString());
+		response.setStatus(HttpServletResponse.SC_OK);
+		response.setCharacterEncoding("utf-8");
+		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+		response.getWriter().write(objectMapper.writeValueAsString(null));
+	}
+}
+
