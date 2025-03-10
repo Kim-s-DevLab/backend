62a74365ab13f7565c1e9d19f9277e9053c7168d
ijnim1121 <jimin3347@naver.com>
2025-03-10 22:04:40 +0900

    feat: 엔티티 작성

---
+++ src/main/java/eightplusone/bit/fit/domain/chat/entity/ChatLike.java
@@ -0,0 +1,31 @@
+package eightplusone.bit.fit.domain.chat.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatLike {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long likeId; // 좋아요 PK
+
+	private Long userId; // 사용자 아이디
+
+	@ManyToOne
+	@JoinColumn(name = "message_id", nullable = false)
+	private ChatMessage chatMessage; // 메시지 PK (외래키)
+
+}
+
---
+++ src/main/java/eightplusone/bit/fit/domain/chat/entity/ChatMessage.java
@@ -0,0 +1,39 @@
+package eightplusone.bit.fit.domain.chat.entity;
+
+import java.time.LocalDateTime;
+
+import eightplusone.bit.fit.domain.chat.enums.ChatCategory;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatMessage {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long messageId; // 메시지 PK
+
+	private Long lectureId; // 강연 ID
+
+	private Long userId; // 사용자 ID
+
+	@Enumerated(EnumType.STRING)
+	private ChatCategory category; // ENUM 사용
+
+	@Column(length = 300, nullable = false)
+	private String message; // 채팅 내용
+
+	private LocalDateTime timestamp; // 작성 시간
+}
