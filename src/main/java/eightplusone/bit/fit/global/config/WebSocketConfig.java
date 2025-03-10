02fe7928bd775de910f44fb09eb580341ce2bdad
ijnim1121 <jimin3347@naver.com>
2025-03-10 22:09:35 +0900

    feat: sockJS 제거

--- src/main/java/eightplusone/bit/fit/global/config/WebSocketConfig.java
+++ src/main/java/eightplusone/bit/fit/global/config/WebSocketConfig.java
@@ -14,10 +14,11 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	public void configureMessageBroker(MessageBrokerRegistry config) {
 		config.enableSimpleBroker("/sub"); // 구독 경로
 		config.setApplicationDestinationPrefixes("/pub"); // 발행 경로
+		System.out.println("✅ WebSocket 메시지 브로커 설정 완료");
 	}
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/ws-chat").setAllowedOrigins("*").withSockJS();
+		registry.addEndpoint("/ws-chat").setAllowedOriginPatterns("*");
 	}
 }
