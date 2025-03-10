6dbc9b9a4b228280db144bf0eea819d79832bbc0
ijnim1121 <jimin3347@naver.com>
2025-03-10 22:04:51 +0900

    feat: DTO 작성

---
+++ src/main/java/eightplusone/bit/fit/domain/chat/dto/ChatMessageDto.java
@@ -0,0 +1,11 @@
+package eightplusone.bit.fit.domain.chat.dto;
+
+import eightplusone.bit.fit.domain.chat.enums.ChatCategory;
+import lombok.Data;
+
+@Data
+public class ChatMessageDto {
+	private Long userId;
+	private ChatCategory category;
+	private String message;
+}
