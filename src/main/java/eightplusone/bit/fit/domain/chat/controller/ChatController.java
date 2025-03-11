e7eaf470bcc33888d8d27872d4a5d571e6768844
leedidu <dlwltn510@gmail.com>
2025-03-11 17:26:08 +0900

    feat: uri 및 헤더 변경

--- src/main/java/eightplusone/bit/fit/domain/chat/controller/ChatController.java
+++ src/main/java/eightplusone/bit/fit/domain/chat/controller/ChatController.java
@@ -2,35 +2,35 @@ package eightplusone.bit.fit.domain.chat.controller;
 
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+
 import eightplusone.bit.fit.domain.chat.dto.ChatMessageDto;
 import eightplusone.bit.fit.domain.chat.service.ChatService;
-import eightplusone.bit.fit.global.websocket.WebSocketEventListener;
 
 @RestController
 @RequestMapping("/chat")
 public class ChatController {
 	private final ChatService chatService;
-	private static final Logger log = LoggerFactory.getLogger(WebSocketEventListener.class);
 
 	public ChatController(ChatService chatService) {
 		this.chatService = chatService;
 	}
 
 	// Redis로 발행
-	@MessageMapping("/sendMessage")
-	public void sendMessage(@Payload ChatMessageDto message, @RequestHeader("User-Id") String userId) {
-		log.info("✅ WebSocket 메시지 수신: {}", message);
-		chatService.sendMessage(message, userId);
+	@MessageMapping("/chat/{sessionId}")
+	public void sendMessage(@DestinationVariable("sessionId") String sessionId,
+		@Payload ChatMessageDto message,
+		@Header("User-Id") String userId) throws JsonProcessingException {
+		chatService.sendMessage(message, userId, sessionId);
 	}
 
 	// 최근 채팅 메시지 조회 (REST API)
