d91d42346127b4efe02652a1faa9f6d44988b27f
leedidu <dlwltn510@gmail.com>
2025-03-11 13:38:44 +0900

    feat: (임시) 코드 수정

--- src/main/java/eightplusone/bit/fit/domain/chat/service/ChatService.java
+++ src/main/java/eightplusone/bit/fit/domain/chat/service/ChatService.java
@@ -1,35 +1,33 @@
 package eightplusone.bit.fit.domain.chat.service;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
-import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import eightplusone.bit.fit.domain.chat.dto.ChatMessageDto;
-import eightplusone.bit.fit.domain.chat.entity.ChatMessage;
 import eightplusone.bit.fit.domain.chat.repository.ChatRepository;
 
 @Service
 public class ChatService {
 	private final ChatRepository chatRepository;
-	private final StringRedisTemplate redisTemplate;
+	private final RedisTemplate<String, Object> redisTemplate;
 
-	public ChatService(ChatRepository chatRepository, StringRedisTemplate redisTemplate) {
+	public ChatService(ChatRepository chatRepository, RedisTemplate<String, Object> redisTemplate) {
 		this.chatRepository = chatRepository;
 		this.redisTemplate = redisTemplate;
 	}
 
 	// 메시지 전송 (Redis Pub/Sub 사용)
-	public void sendMessage(ChatMessageDto dto, Long userId) {
-		ChatMessage message = new ChatMessage();
-		message.setUserId(userId);
-		message.setCategory(dto.getCategory());
-		message.setMessage(dto.getMessage());
-		message.setTimestamp(LocalDateTime.now());
+	public void sendMessage(ChatMessageDto dto, String userId) {
+		// ChatMessage message = new ChatMessage();
+		// message.setUserId(userId);
+		// message.setCategory(dto.getCategory());
+		// message.setMessage(dto.getMessage());
+		// message.setTimestamp(LocalDateTime.now());
 
 		// DB 저장
-		chatRepository.saveMessage(message);
+		// chatRepository.saveMessage(message);
 
 		// "userId|category|message" 형식으로 변환하여 Redis에 전송
 		String redisMessage = userId + "|" + dto.getCategory() + "|" + dto.getMessage();
