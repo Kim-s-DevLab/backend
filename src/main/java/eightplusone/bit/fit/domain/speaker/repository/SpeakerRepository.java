5530b8b4709e483dd9e08b467c2966464e8ffe34
leedidu <dlwltn510@gmail.com>
2025-03-17 11:31:32 +0900

    feat: speaker 엔티티 설정

---
+++ src/main/java/eightplusone/bit/fit/domain/speaker/repository/SpeakerRepository.java
@@ -0,0 +1,8 @@
+package eightplusone.bit.fit.domain.speaker.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import eightplusone.bit.fit.domain.speaker.entity.Speaker;
+
+public interface SpeakerRepository extends JpaRepository<Speaker, Long> {
+}
---
+++ src/main/java/eightplusone/bit/fit/domain/speaker/service/SpeakerService.java
@@ -0,0 +1,7 @@
+package eightplusone.bit.fit.domain.speaker.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SpeakerService {
+}
