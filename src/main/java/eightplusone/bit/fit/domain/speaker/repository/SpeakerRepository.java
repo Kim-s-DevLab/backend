d520338a429f518cbaeef7cea4f70aff9e4d871d
leedidu <dlwltn510@gmail.com>
2025-03-18 14:00:53 +0900

    feat: 세션아이디를 이용하여 태그, 연사 찾는 메소드 추가

--- src/main/java/eightplusone/bit/fit/domain/speaker/repository/SpeakerRepository.java
+++ src/main/java/eightplusone/bit/fit/domain/speaker/repository/SpeakerRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import eightplusone.bit.fit.domain.speaker.entity.Speaker;
 
 public interface SpeakerRepository extends JpaRepository<Speaker, Long> {
+	Speaker findBySession_SessionId(Long sessionId);
 }
