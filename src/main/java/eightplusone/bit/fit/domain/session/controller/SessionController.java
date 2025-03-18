d511f697ed1094530aac2d5a6704a7b1334f7959
leedidu <dlwltn510@gmail.com>
2025-03-18 09:26:58 +0900

    feat: 컨트롤러 작성

---
+++ src/main/java/eightplusone/bit/fit/domain/session/controller/SessionController.java
@@ -0,0 +1,36 @@
+package eightplusone.bit.fit.domain.session.controller;
+
+import static org.springframework.http.HttpStatus.*;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import eightplusone.bit.fit.domain.session.dto.SessionListResponseDto;
+import eightplusone.bit.fit.domain.session.service.SessionService;
+import eightplusone.bit.fit.global.dto.ResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/session")
+@RequiredArgsConstructor
+public class SessionController {
+
+	private final SessionService sessionService;
+
+	@Operation(summary = "세션 전체 조회", description = "**성공 응답 데이터:**  세션 전체 리스트")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "세션 전체 조회 성공"),
+	})
+	@GetMapping("/all")
+	public ResponseEntity<ResponseDto<List<SessionListResponseDto>>> all() {
+		return ResponseEntity.status(OK)
+			.body(ResponseDto.success(OK, "세션 전체 리스트 조회 성공", sessionService.getSessionsList()));
+	}
+}
