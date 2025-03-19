d4c62506a5efb2614effab9318696c8c6c84325f
조영무 <fprh13@gmail.com>
2025-03-19 16:07:25 +0900

    chore: 누락된 cors 인증 헤더 추가 #35

--- src/main/java/eightplusone/bit/fit/global/constants/CorsConstant.java
+++ src/main/java/eightplusone/bit/fit/global/constants/CorsConstant.java
@@ -6,7 +6,8 @@ public class CorsConstant {
 
 	public static final String AUTHORIZATION = "authorization";
 	public static final List<String> ALLOWED_METHODS = List.of("GET", "POST", "PUT", "DELETE", "OPTIONS");
-	public static final List<String> ALLOWED_HEADERS = List.of("X-Requested-With", "Content-Type", "Accept");
+	public static final List<String> ALLOWED_HEADERS = List.of("X-Requested-With", "Content-Type", "Accept",
+		"Authorization");
 	public static final boolean ALLOWED_CREDENTIALS = true;
 	public static final long MAX_AGE = 3600L;
 }
