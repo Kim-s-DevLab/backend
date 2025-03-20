29189d996a94ea31439bd5b978068e674f80e38c
leedidu <dlwltn510@gmail.com>
2025-03-20 15:39:17 +0900

    refactor: 안쓰는 메소드 정리

--- src/main/java/eightplusone/bit/fit/domain/session/repository/SessionRepository.java
+++ src/main/java/eightplusone/bit/fit/domain/session/repository/SessionRepository.java
@@ -1,12 +1,10 @@
 package eightplusone.bit.fit.domain.session.repository;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import eightplusone.bit.fit.domain.session.entity.Session;
 
@@ -14,7 +12,4 @@ public interface SessionRepository extends JpaRepository<Session, Long>, Session
 	Optional<Session> findByAudioChannel(Integer audioChannel);
 
 	Page<Session> findAll(Pageable pageable);
-
-	@Query("SELECT s FROM Session s WHERE s.startTime <= CURRENT_TIMESTAMP AND s.endTime >= CURRENT_TIMESTAMP")
-	List<Session> findLiveSessions();
 }
