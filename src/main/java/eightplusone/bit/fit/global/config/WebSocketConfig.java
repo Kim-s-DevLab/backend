14478f95af27463197111271d18b652917afee93
Hm-source <swo8852@gmail.com>
2025-03-23 00:58:58 +0900

    feat: withSocketJS() 라는 함수 활성화

--- src/main/java/eightplusone/bit/fit/global/config/WebSocketConfig.java
+++ src/main/java/eightplusone/bit/fit/global/config/WebSocketConfig.java
@@ -1,13 +1,14 @@
 package eightplusone.bit.fit.global.config;
 
-import eightplusone.bit.fit.domain.auth.interceptor.WebSocketInterceptor;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import eightplusone.bit.fit.domain.auth.interceptor.WebSocketInterceptor;
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -24,7 +25,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/ws").setAllowedOriginPatterns("*");
+		registry.addEndpoint("/ws")
+			.setAllowedOriginPatterns("*")
+			.withSockJS();
 	}
 
 	@Override
