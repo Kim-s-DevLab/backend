da5a5803d97e7caa3be837440c21d33174d7efa9
leedidu <dlwltn510@gmail.com>
2025-03-25 11:35:24 +0900

    feat: SessionDetailResponseDto 작성

---
+++ src/main/java/eightplusone/bit/fit/domain/session/dto/SessionDetailResponseDto.java
@@ -0,0 +1,43 @@
+package eightplusone.bit.fit.domain.session.dto;
+
+import eightplusone.bit.fit.domain.session.entity.Session;
+import eightplusone.bit.fit.domain.speaker.dto.SpeakerResponseDto;
+import eightplusone.bit.fit.domain.tag.dto.TagDto;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@Schema(name = "SessionDetailResponseDto: 세션 상세 조회 Dto")
+public class SessionDetailResponseDto {
+	@Schema(description = "", example = "")
+	private String title;
+
+	@Schema(description = "", example = "")
+	private String summary;
+
+	private SpeakerResponseDto speaker;
+
+	private TagDto tags;
+
+	private Boolean isLiked;
+
+	private Integer likes;
+
+	private Boolean isUnliked;
+
+	private Integer unlikes;
+
+	public static SessionDetailResponseDto from(Session session, SpeakerResponseDto speaker, TagDto tags,
+		Boolean isLiked, Integer likes) {
+		return SessionDetailResponseDto.builder()
+			.title(session.getTitle())
+			.summary(session.getSummary())
+			.isLiked(isLiked)
+			.likes(likes)
+			.speaker(speaker)
+			.tags(tags)
+			.build();
+	}
+}
