293dbb64a58e0161f5e488074d69009b17fd09d3
ijnim1121 <jimin3347@naver.com>
2025-03-26 15:52:06 +0900

    fix: Raw WebSocket 연결용 추가

--- src/main/java/eightplusone/bit/fit/global/config/WebSocketConfig.java
+++ src/main/java/eightplusone/bit/fit/global/config/WebSocketConfig.java
@@ -1,14 +1,13 @@
 package eightplusone.bit.fit.global.config;
 
+import eightplusone.bit.fit.domain.auth.interceptor.WebSocketInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-
-import eightplusone.bit.fit.domain.auth.interceptor.WebSocketInterceptor;
-import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -25,6 +24,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		// Raw WebSocket 연결용
+		registry.addEndpoint("/ws")
+			.setAllowedOriginPatterns("*");
+		// SockJS fallback 용
 		registry.addEndpoint("/ws")
 			.setAllowedOriginPatterns("*")
 			.withSockJS();
