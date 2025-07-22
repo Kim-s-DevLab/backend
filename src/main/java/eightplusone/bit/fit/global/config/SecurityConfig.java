578e7dc2c71b1f727f16254550addfe01a7f1c55
ijnim1121 <jimin3347@naver.com>
2025-07-22 12:15:22 +0900

    refactor: 경로 변경

--- src/main/java/eightplusone/bit/fit/global/config/SecurityConfig.java
+++ src/main/java/eightplusone/bit/fit/global/config/SecurityConfig.java
@@ -84,8 +84,8 @@ public class SecurityConfig {
 			.httpBasic(AbstractHttpConfigurer::disable)
 			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 			.authorizeHttpRequests(auth -> auth
-				.requestMatchers("/actuator/prometheus").permitAll()
-				.requestMatchers("/actuator/**").permitAll()
+				.requestMatchers("/fit-monitor/prometheus").permitAll()
+				.requestMatchers("/fit-monitor/**").permitAll()
 				.requestMatchers(PUBLIC_GET.getMethod(), PUBLIC_GET.getPaths())
 				.permitAll()
 				.requestMatchers(PUBLIC_POST.getMethod(), PUBLIC_POST.getPaths())
